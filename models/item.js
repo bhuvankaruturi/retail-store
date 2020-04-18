9d8242e9964a115e388f73e421a998a6e809d71b
Bhuvan Karuturi <bhuvankaruturi123@gmail.com>
2020-04-18 15:55:09 -0500

    added image upload

--- models/item.js
+++ models/item.js
@@ -4,6 +4,7 @@ var Item = mongoose.Schema({
     itemname: {type: String, required: true},
     description: {type: String},
     price: {type: Number},
+    imageUrl: {type: String, default: '/images/default.jpg'},
     sizes: [
         {
             type: mongoose.Schema.Types.ObjectId, 
--- routes/items.js
+++ routes/items.js
@@ -1,9 +1,27 @@
+var fs = require('fs');
 var express = require('express');
 var mongoose = require('mongoose');
+var path = require('path');
+// multer for image upload
+var multer  = require('multer')
+// mongoose models
 var Item = require('../models/item');
 var Size = require('../models/size');
+// valid size for item
 var sizes = require('../util/sizes');
+
 var router = express.Router();
+
+// multer configuration
+var storage = multer.diskStorage({
+    destination: path.join(path.resolve(__dirname, '..'), '/public/images/items'), 
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      const extension = file.originalname.match(/\.[0-9a-z]+$/i)[0];
+      cb(null, file.fieldname + '-' + uniqueSuffix + extension);
+    }
+});
+var upload = multer({ storage: storage})
 
 // TODO: replace json response with res.render
 router.get('/', function(req, res, next) {
@@ -16,13 +34,25 @@ router.get('/', function(req, res, next) {
     });
 });
 
+// TODO: replace json response with res.render
+router.get('/:id', function(req, res, next) {
+    Item.findById(req.params.id, function(err, doc) {
+        if (err) {
+            err = "Something went wrong while fetching the item";
+            return next(err);
+        }
+        return res.status(200).json(doc);
+    });
+})
+
 // Create a new item with all item size of count 1
-router.post('/add', function(req, res, next){
+router.post('/add', upload.single('item_image'), function(req, res, next){
     var item = new Item({
         _id: new mongoose.Types.ObjectId(),
         itemname: req.body.itemname,
         description: req.body.description || "",
-        price: req.body.price
+        price: req.body.price,
+        imageUrl: req.file.filename || 'default.jpg'
     });
     var itemSizes = [];
     for (var size of sizes) {
@@ -51,8 +81,15 @@ router.post('/add', function(req, res, next){
 });
 
 // Update a item
-router.put('/:id', function(req, res, next) {
-    Item.findByIdAndUpdate(req.params.id, req.body.item, function(err, item){
+router.put('/:id', upload.single('item_image'), function(req, res, next) {
+    if (req.file) {
+        fs.unlink(path.join(path.resolve(__dirname, '..'), '/public/images/items/', req.body.imageUrl), function(err) {
+            if (err) console.log(err);
+        });
+        req.body.imageUrl = req.file.filename;
+    }
+    console.log(req.body);
+    Item.findByIdAndUpdate(req.params.id, req.body, function(err, item){
         if (err) {
             err = "Something went wrong while updating the item";
             return next(err);
@@ -63,13 +100,24 @@ router.put('/:id', function(req, res, next) {
 
 // Delete a item
 router.delete('/:id', function(req, res, next) {
-    Item.findByIdAndDelete(req.params.id, function(err) {
+    Item.findById(req.params.id, function(err, doc){
         if (err) {
             err = "Something went wrong while deleting the item";
             return next(err);
         }
-        res.redirect('/items');
-    })
+        if (doc.get('imageUrl') && doc.get('imageUrl') !== 'default.jpg') {
+            fs.unlink(path.join(path.resolve(__dirname, '..'), '/public/images/items/', doc.get('imageUrl')), function(err) {
+                if (err) console.log(err);
+            });
+        }   
+        doc.remove(function(err) {
+            if (err) {
+                err = "Something went wrong while deleting the item";
+                return next(err);
+            }
+            res.redirect('/items');
+        })
+    });
 })
 
 module.exports = router;
