4cbbbd58b49245912e5d9859d6f2c015ec92a1d9
Bhuvan Karuturi <bhuvankaruturi123@gmail.com>
2020-04-18 13:21:07 -0500

    items route added

--- models/item.js
+++ models/item.js
@@ -3,7 +3,13 @@ var mongoose = require('mongoose');
 var Item = mongoose.Schema({
     itemname: {type: String, required: true},
     description: {type: String},
-    price: {type: Number}
+    price: {type: Number},
+    sizes: [
+        {
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: 'Size'
+        }
+    ]
 });
 
 module.exports = mongoose.model('Item', Item);
--- models/size.js
+++ models/size.js
@@ -1,9 +1,19 @@
 var mongoose = require('mongoose');
 
 var Size = mongoose.Schema({
-    itemid: {type: mongoose.Schema.Types.ObjectId, ref: 'Item', required: true},
-    size: {type: String, required: true},
-    count: {type: Number, default: 1}
+    itemid: {
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Item', 
+        required: true
+    },
+    size: {
+        type: String, 
+        required: true
+    },
+    count: {
+        type: Number, 
+        default: 1
+    }
 });
 
 module.exports = mongoose.model('Size', Size);
